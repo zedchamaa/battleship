f52c82f83d45d7f96d4385464a41a89b657fc0f7
zedchamaa <sales@nineprovince.com>
2022-07-11 10:47:19 +0200

    Create the view object to keep the display updated

---
+++ battleship.js
@@ -0,0 +1,16 @@
+// Create the view object to keep the display updated with hits, misses and messages for the user
+
+const view = {
+    displayMessage: function(msg) {
+        const messageArea = document.getElementById("messageArea");
+        messageArea.innerHTML = msg;
+    },
+    displayHit: function(location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "hit");
+    },
+    displayMiss: function (location) {
+        const cell = document.getElementById(location);
+        cell.setAttribute("class", "miss");
+    }
+}
