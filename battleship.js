99be3e7d026802670c33373b11dbb33935b4892a
zedchamaa <sales@nineprovince.com>
2022-07-13 10:12:39 +0200

    Create the controller object to manage the player's guesses

--- battleship.js
+++ battleship.js
@@ -53,3 +53,26 @@ const model = {
         return true;
     }
 }
+
+// Create the controller object to manage the player's guesses
+
+function parseGuess(guess) {
+    var alphabet = ["A", "B", "C", "D", "E", "F", "G"];
+
+    if (guess === null || guess.length !== 2) {
+        alert("Oops, please enter a letter and a number on the board.");
+    } else {
+        var firstChar = guess.charAt(0);
+        var row = alphabet.indexOf(firstChar); 
+        var column = guess.charAt(1);
+
+        if (isNaN(row) || isNaN(column)) {
+            alert("Oops, that isn't on the board.");
+        } else if (row < 0 || row >= model.boardSize || column < 0 || column >= model.boardSize) {
+            alert("Oops, that's off the board!");
+        } else {
+            return row + column;
+        }
+    }
+    return null;
+}
